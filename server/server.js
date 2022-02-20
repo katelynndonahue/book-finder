cb580165eb88b3f77d5233f82e15f317abc9f7fe
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-02-20 15:23:28 -0500

    edits to server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
+const routes = require('./routes');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -12,19 +12,16 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
-
 server.applyMiddleware({ app });
-
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
