a3daea34a2181ec06344e183edd625fa050eb8a1
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-02-22 14:48:18 -0500

    modifications to file structure and data

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+    me: User
+  }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(userId: ID!, bookId: ID!): User
+    removeBook(bookId: ID!): User
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]!
+  }
+  type Book {
+    bookId: String
+    authors: [String]!
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+module.exports = typeDefs;
