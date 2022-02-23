59bc96e79895eda791ef9f1c32764a1556c1b413
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-02-22 22:52:35 -0500

    edits to controller and typeDefs

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,12 +1,44 @@
-const { Tech } = require('../models');
+const { Book, allBooks } = require('../models');
 
 module.exports = {
-  async getAllTech(req, res) {
-    const allTech = await Tech.find({});
+  async getAllBooks(req, res) {
+    const allBooks = await Book.find({});
 
-    if (!allTech) {
-      return res.status(400).json({ message: 'No technologies found' });
+    if (!allBooks) {
+      return res.status(400).json({ message: 'No books found' });
     }
-    res.status(200).json(allTech);
+    res.status(200).json(allBooks);
   },
 };
+async searchBooks(req, res) {
+  const book = await Matchup.findOneAndUpdate(
+    { _id: req.body.id },
+    {$inc: { [`book${req.body.bookName}_search`]: "" } },
+    {new: true }
+  );
+
+  if (!search) {
+    return res.status(400).json({ message: 'Unable to find book' });
+  }
+  res.status(200).json(search);
+},
+
+async getAllBooks(req, res) {
+  const allBooks = await allBooks.find({});
+
+  if (!allBooks) {
+    return res.status(400).json({ message: 'No books found' });
+  }
+
+  res.status(200).json(allBooks);
+},
+async getAllBooks({ params }, res) {
+  const allBooks = await allBooks.findOneAnd({ _id: params.id });
+
+  if (!allBooks) {
+    return res.status(400).json({ message: 'No books found by that id' });
+  }
+
+  res.status(200).json(allBooks);
+},
+}
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,36 +2,45 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   
-  type Query {
-    users: [User]!
-    user(userId: ID!): User
-    me: User
-  }
-  type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
-    saveBook(userId: ID!, bookId: ID!): User
-    removeBook(bookId: ID!): User
-  }
   type User {
-    _id: ID
-    username: String
-    email: String
-    bookCount: Int
+    _id: Id
+    username: String!
+    email: String!
+    password: String!
     savedBooks: [Book]!
   }
   type Book {
-    bookId: String
     authors: [String]!
-    description: String
-    title: String
+    description: String!
+    bookId: Id!
+    title: String!
     image: String
     link: String
   }
   type Auth {
-    token: ID!
+    token: Id!
     user: User
   }
+  input BookInput{
+    authors: [String]
+    description: String!
+    title: String!
+    bookId: String!
+    link: String
+    image:String
+}
+type Query {
+  users: [User]
+  user(id: Id!): User
+  me: User
+}
+type Mutation {
+  addUser(username: String!, email: String!, password: String!): Auth login(email: String!, password: String!): Auth
+ 
+  saveBook (
+  bookToSave: BookInput!):User
+  removeBook(bookId: String): User
+}
 `;
 
 module.exports = typeDefs;
